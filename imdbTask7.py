d2adfaf170e1d37067648c10155c834a7e79c311
NEHAISRANI <nehai18@navgurukul.org>
2020-04-03 23:51:08 +0530

    in this task accaccording to the directors one directors how much movies he has made that i count

---
+++ imdbTask7.py
@@ -0,0 +1,22 @@
+from imdbTask5 import *
+from pprint import pprint
+
+def directorsName(movieDirectors):
+    directorsList=[]
+    for i in movieDirectors:
+        director=i["Directors"]
+        directorsList.extend(director) 
+    return(directorsList)
+allDirectorsName=directorsName(movie_details)
+# print(allDirectorsName)
+
+def analyse_movies_directors(Directors):
+    directorsDic={}
+    for i in Directors:
+        if i not in directorsDic:
+            directorsDic[i]=1
+        else:
+            directorsDic[i]+=1
+    return(directorsDic)
+data=analyse_movies_directors(allDirectorsName)
+# print(data)
