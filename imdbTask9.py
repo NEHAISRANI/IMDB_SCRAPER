2eadc7a146587a5b3c3c62a9a57d00f16c3f7809
NEHAISRANI <nehai18@navgurukul.org>
2020-04-20 21:04:10 +0530

    in this task i have done caching.caching is that i imported task1 from that task taking movies url.if file is exists then it will read that file and will get movies data from there but if file is not exists then it will create that file dump that data in that file.i have set time also randoint randomly it will take automatically one number within that number it will give data.

---
+++ imdbTask9.py
@@ -0,0 +1,30 @@
+import time
+import random
+from pprint import pprint
+from imdbTask1 import*
+from imdbTask4 import*
+import os 
+import json 
+def moviesCaching():
+    getDetails=[]
+    for i in moviesInfo:
+        link=i["url"]
+        movieUrl=link.split("/")
+        fileName1=("cach/"+str(movieUrl[5])+".json")
+        if os.path.exists(fileName1):
+            # print("-------------------------------")
+            file=open(fileName1,"r")
+            readFile=file.read()
+            getDetails.append(readFile)
+            file.close()  
+        else:
+            functionCall=Scrap_Movie_Details(link) 
+            with open("cach/"+str(movieUrl[5])+".json","w") as file:
+                write=json.dump(functionCall,file)
+                timeLimit=random.randint(1,3)
+                time.sleep(timeLimit)
+    return(getDetails)
+movieDetailsWithCaching=moviesCaching() 
+pprint(movieDetailsWithCaching)
+# print(len(movieDetailsWithCaching))
+
